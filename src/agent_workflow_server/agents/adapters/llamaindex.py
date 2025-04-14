1b4bec8d0c8ef8e6f7378533011a2b0aae015462
Marco Trinelli (mtrinell) <mtrinell@cisco.com>
2025-04-14 16:59:18 +0200

    fix: handle state
    Signed-off-by: Marco Trinelli (mtrinell) <mtrinell@cisco.com>

--- src/agent_workflow_server/agents/adapters/llamaindex.py
+++ src/agent_workflow_server/agents/adapters/llamaindex.py
@@ -6,7 +6,12 @@ import inspect
 import json
 from typing import Any, Dict, Optional
 
-from llama_index.core.workflow import Context, Workflow
+from llama_index.core.workflow import (
+    Context,
+    HumanResponseEvent,
+    InputRequiredEvent,
+    Workflow,
+)
 from llama_index.core.workflow.context_serializers import (
     JsonPickleSerializer,
 )
@@ -35,12 +40,7 @@ class LlamaIndexAdapter(BaseAdapter):
 
 
 class WorkflowState(BaseModel):
-    """Holds the state of the workflow.
-
-    Used to validate and pass message payloads.
-
-    TODO: Should this be the general payload for all messages?
-    """
+    """Holds the state of the workflow."""
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
@@ -52,25 +52,19 @@ class WorkflowState(BaseModel):
         default_factory=dict, description="Run kwargs needed to run the workflow."
     )
     session_id: Optional[str] = Field(
-        default=None, description="Session ID for the current task."
+        default=None, description="Session ID for the current Run"
     )
-    task_id: str = Field(description="Task ID for the current run.")
 
 
 class LlamaIndexAgent(BaseAgent):
     def __init__(self, agent: Workflow):
         self.agent = agent
+        self.sessions_state: Dict[str, str] = {}
 
-    async def get_workflow_state(self, state: WorkflowState) -> Optional[Context]:
-        """Load the existing context from the workflow state."""
-        if state.session_id is None:
-            return None
+    def _get_ctx(self, session_id: str) -> Optional[Context]:
+        """Load the existing context from the workflow state, if any."""
 
-        state_dict = await self.get_session_state(state.session_id)
-        if state_dict is None:
-            return None
-
-        workflow_state_json = state_dict.get(state.session_id, None)
+        workflow_state_json: Optional[str] = self.sessions_state.get(session_id, None)
 
         if workflow_state_json is None:
             return None
@@ -84,7 +78,7 @@ class LlamaIndexAgent(BaseAgent):
         context_hash = _make_hash(context_str)
 
         if workflow_state.hash is not None and context_hash != workflow_state.hash:
-            raise ValueError("Context hash does not match!")
+            raise ValueError("Context hash does not match.")
 
         return Context.from_dict(
             self.agent,
@@ -92,9 +86,12 @@ class LlamaIndexAgent(BaseAgent):
             serializer=JsonPickleSerializer(),
         )
 
-    async def set_workflow_state(
-        self, ctx: Context, current_state: WorkflowState
+    def _set_ctx(
+        self, ctx: Context, session_id: str, run_kwargs: Dict[str, Any]
     ) -> None:
+        if session_id is None:
+            raise ValueError("Session ID is None. Cannot set workflow state.")
+
         """Set the workflow state for this session."""
         context_dict = ctx.to_dict(serializer=JsonPickleSerializer())
         context_str = json.dumps(context_dict)
@@ -103,34 +100,50 @@ class LlamaIndexAgent(BaseAgent):
         workflow_state = WorkflowState(
             hash=context_hash,
             state=context_dict,
-            run_kwargs=current_state.run_kwargs,
-            session_id=current_state.session_id,
-            task_id=current_state.task_id,
+            run_kwargs=run_kwargs,
+            session_id=session_id,
         )
 
-        if current_state.session_id is None:
-            raise ValueError("Session ID is None! Cannot set workflow state.")
-
-        session_state = await self.get_session_state(current_state.session_id)
-        if session_state:
-            session_state[current_state.session_id] = workflow_state.model_dump_json()
-
-            # Store the state in the control plane
-            await self.update_session_state(current_state.session_id, session_state)
+        session_state = workflow_state.model_dump_json()
+        self.sessions_state[session_id] = session_state
 
     async def astream(self, run: Run):
         input = run["input"]
 
-        ctx = await self.get_workflow_state(run)
+        ctx = self._get_ctx(run["thread_id"])
 
         handler = self.agent.run(ctx=ctx, input={**input})
+        if handler.ctx is None:
+            # This should never happen, workflow.run actually sets the Context
+            raise ValueError("Context cannot be None.")
+
         async for event in handler.stream_events():
-            yield Message(
-                type="message",
-                data=event,
-            )
+            if isinstance(event, InputRequiredEvent):
+                # If I have a user_data, I send it to the workflow
+                if "interrupt" in run and "user_data" in run["interrupt"]:
+                    user_data = run["interrupt"]["user_data"]
+
+                    # FIXME: workaround to extract the user response from a dict/obj. Needed for input validation, remove once not needed anymore.
+                    if isinstance(user_data, dict):
+                        user_data = list(user_data.values())[0]
+                    handler.ctx.send_event(HumanResponseEvent(response=user_data))
+                    # TODO: maybe need to delete user_data from the run
+                else:
+                    # otherwise, I send the interrupt
+                    # FIXME: workaround to wrap the prefix (str) in a dict/obj. Needed for output validation, remove once not needed anymore.
+                    yield Message(type="interrupt", data={"interrupt": event.prefix})
+            else:
+                yield Message(
+                    type="message",
+                    data=event,
+                )
         final_result = await handler
         yield Message(
             type="message",
             data=final_result,
+        )
+        self._set_ctx(
+            ctx=handler.ctx,
+            session_id=run["thread_id"],
+            run_kwargs={**input},
         )
